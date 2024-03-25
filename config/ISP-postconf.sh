a1cab1b15dcd7e0ce13635857ca151158b42432a
Ivashkka <ivashka.2.r@gmail.com>
2024-03-25 21:35:25 +0300

    NM setup

--- config/ISP-postconf.sh
+++ config/ISP-postconf.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 rm -rf /etc/NetworkManager/system-connections/Пр*
+chown root:root /etc/NetworkManager/system-connections/*.nmconnection
+chmod 600 /etc/NetworkManager/system-connections/*.nmconnection
